2a54544b7179346c451e93273975f3d3a74237ef
System Administrator <root@Tests-MacBook-Pro.local>
2021-10-25 16:22:20 +0200

    DataBanding

---
+++ my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  // selector: '[app-servers]',
+  // selector: '.app-servers'
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer: boolean = false;
+  serverCreationStatus: string = 'No server is created';
+  serverName: string = '';
+ 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit(): void {
+  }
+
+  onAddNewServer() {
+    this.serverCreationStatus = "You have just created the server! Server name is:" + this.serverName!;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+}
