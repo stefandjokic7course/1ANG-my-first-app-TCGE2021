1c60dc880be8949e0f1c76a9c69abec3d616596a
System Administrator <root@Tests-MacBook-Pro.local>
2021-10-26 21:13:16 +0200

    directives

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -12,6 +12,11 @@ export class ServersComponent implements OnInit {
   serverCreationStatus: string = 'No server is created';
   serverName: string = '';
   username: string;
+  servernameEntered: boolean = false;
+  servers = ['ServerTest1', 'ServerTest2'];
+  isSecretPasswordVisible: boolean = true;
+  clicks: number[] = [];
+  counter: number = 1;
  
   constructor() {
     setTimeout(() => {
@@ -23,10 +28,24 @@ export class ServersComponent implements OnInit {
   }
 
   onAddNewServer() {
+    this.servernameEntered = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = "You have just created the server! Server name is:" + this.serverName!;
   }
 
   onUpdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  onDisplayDetails() {
+    this.isSecretPasswordVisible = this.isSecretPasswordVisible === true ? false : true;
+    this.clicks.length === 0 ? this.clicks.push(1): this.clicks.push(this.counter);
+    this.counter ++;
+    console.log(this.clicks);
+    
+  }
+
+  getColor() {
+    return this.clicks.length >=5 ? 'blue' : 'white';
   }
 }
